f2df02e12efbc1c214da2664d1aff84c5256e0e2
22zcorr <22zcorr@athenian.org>
2020-10-26 19:50:44 +0000

    I wrote some code

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,80 @@
-public class Person {
+public class Person 
+{
+    private String firstName;
+    private String lastName;
+    private int xp;
+
+    public Person(String a, String b, int c)
+    {
+        firstName = a;
+        lastName = b;
+        xp = c;
+
+    }
     
+    @Override
+    public String toString(){
+        return this.firstName+ " " +this.lastName;
+    }
+
+    public void doSomething(){
+        xp+=3;
+        System.out.println(this.firstName+ " did something.");
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public int getXP(){
+        return this.xp;
+    }
+    public void setFirstName(String a) {
+		if(a.trim().length()>0){this.firstName = a;}
+    }
+    public void setLastName(String a) {
+		if(a.trim().length()>0){this.lastName = a;}
+    }
+    
+    public void cook(){
+        xp+=5;
+        int randomDish = ((int)(Math.random()*4))+1;
+
+        if(randomDish==1){
+            System.out.println( this.toString() + " has baked apples, cinnamon, and sugar into a warm Apple Pie!");
+        }
+        else if(randomDish==2){
+            System.out.println( this.toString() + " has blended various fruits into a Smoothie!");
+        }
+        else if(randomDish==3){
+            System.out.println( this.toString() + " has sautéed some vegetables, meat, egg, and rice into Bibimbap!");
+        }
+        else{
+            System.out.println( this.toString() + " has boiled some rice cakes and mixed it with red chili pepper paste to make Tteokbokki");
+        }
+    }
+
+    public void nothing(){
+        xp+=1;
+        System.out.println(this.toString()+ " did nothing. (Why did you choose this one?)");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class PersonClient {
     public static void main(String[] args) {
-        
+             
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name? (input as [First] [Last])");
+        String scan = input.nextLine();
+        String first = scan.substring(0,scan.indexOf(" "));
+        String last = scan.substring(scan.indexOf(" ")+1);
+        Person p = new Person(first, last, 0);
+        System.out.println("Type \"STOP\" at any point to stop the game.");
+
+        while(true){
+            System.out.println("What should "+p.toString()+" do? \n1) Cook\n2) Do Something\n3) Nothing\n(1,2,3)");
+            String action = input.nextLine();
+            if(action.equals("1")){p.cook();}
+            else if(action.equals("2")){p.doSomething();}
+            else if(action.equals("STOP")){
+                System.out.println(p.toString()+"\'s final XP was "+p.getXP());
+                break;
+            }
+            else{p.nothing();}
+            System.out.println(p.getFirstName()+"\'s XP is "+p.getXP());
+        }
+
+
+
+
+
+
+        input.close();
     }
 }
